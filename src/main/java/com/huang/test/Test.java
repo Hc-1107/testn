f411eec3528bd9435cc4fc842aa52bebbc1ded70
HuangChong <hchong1107@163.com>
2024-05-20 14:23:39 +0800

    test仓库

---
+++ src/main/java/com/huang/test/Test.java
@@ -0,0 +1,11 @@
+package com.huang.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+
+    @Value("${rabbitmq.host}")
+    private String host;
+}
